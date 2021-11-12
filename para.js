25edb1d50f83273ec8df493221b6d3debac4b1f2
Thomas Quinton <91420588+SleepFast@users.noreply.github.com>
2021-11-12 15:40:20 +0100

    Add files via upload

--- para.js
+++ para.js
@@ -5,6 +5,7 @@ let lune = document.getElementById('lune');
 let subtitle = document.getElementById('subtitle');
 let pouet = document.getElementById('pouet');
 let btn = document.getElementById('btn');
+let header = document.getElementById('header');
 
 window.addEventListener('scroll', function () {
     let scroll = window.scrollY;
@@ -15,6 +16,7 @@ window.addEventListener('scroll', function () {
     subtitle.style.marginRight = scroll *3.5 +'px';
     subtitle.style.marginTop = (scroll-120) *1.4 +'px';
     btn.style.marginTop = scroll * 1.5 + 'px';
+    header.style.top = scroll + 'px';
     if (scroll >= 302 ){
         subtitle.style.zIndex = '7';
     }else{
